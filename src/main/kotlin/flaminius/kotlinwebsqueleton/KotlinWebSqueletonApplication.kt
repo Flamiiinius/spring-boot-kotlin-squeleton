7761ca5726b389dbc972ba9c18e15e8b3b6d5258
Flamiiinius <rodrigommfr@gmail.com>
2023-02-05 10:56:32 +0000

    1st commit - generated code

---
+++ src/main/kotlin/flaminius/kotlinwebsqueleton/KotlinWebSqueletonApplication.kt
@@ -0,0 +1,11 @@
+package flaminius.kotlinwebsqueleton
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebSqueletonApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinWebSqueletonApplication>(*args)
+}
---
+++ src/test/kotlin/flaminius/kotlinwebsqueleton/KotlinWebSqueletonApplicationTests.kt
@@ -0,0 +1,13 @@
+package flaminius.kotlinwebsqueleton
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinWebSqueletonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
